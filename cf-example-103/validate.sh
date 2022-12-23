3be4ff6ad4176ef8f851a30bb41eb4fed47e1c90
coryk2049 <cory.kin@gmail.com>
2022-12-23 18:58:31 -0500

    Revisions

--- cf-example-103/validate.sh
+++ cf-example-103/validate.sh
@@ -6,9 +6,8 @@ info() {
     echo -e "[`date '+%m/%d/%Y-%H:%M:%S'`]::INFO::---------------------------------------------------"
 }
 
-YAML_FILES="main vpc sg ec2 alb"
-for FNAME in ${YAML_FILES}; do
-    FULL_FNAME=${FNAME}.yaml
-	info "Checking CF template file: ${FULL_FNAME}"
-    aws cloudformation validate-template --template-body file://${FULL_FNAME}
+
+for FN in $(ls *.yaml); do
+	info "Validating CF template file: ${FULL_FNAME}"
+	aws cloudformation validate-template --template-body file://$FN;
 done
--- cf-example-105/validate.sh
+++ cf-example-105/validate.sh
@@ -1,6 +1,13 @@
-#!/bin/bash 
-for FN in $(ls *.yaml);
-do
-	echo $FN
+#!/bin/bash
+
+info() {
+    echo -e "[`date '+%m/%d/%Y-%H:%M:%S'`]::INFO::---------------------------------------------------"
+    echo -e "[`date '+%m/%d/%Y-%H:%M:%S'`]::INFO::$1"
+    echo -e "[`date '+%m/%d/%Y-%H:%M:%S'`]::INFO::---------------------------------------------------"
+}
+
+
+for FN in $(ls *.yaml); do
+	info "Validating CF template file: ${FN}"
 	aws cloudformation validate-template --template-body file://$FN;
 done
